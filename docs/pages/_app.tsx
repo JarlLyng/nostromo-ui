d8bc4bcb23a00a969eb18c8febb040d93d73095e
Jarl Lyng <jarllyng@gmail.com>
2025-12-13 00:59:46 +0100

    fix: remove CodeBlock import from _app.tsx
    CodeBlock component is available for use in MDX pages
    but should not be imported in _app.tsx. MDX component
    overrides should be handled differently if needed.

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,32 +1,9 @@
 import type { AppProps } from 'next/app'
-import type { MDXComponents } from 'mdx/types'
-import React from 'react'
-import CodeBlock from '../components/CodeBlock'
 import '@nostromo/ui-tw/base.css'
 import '@nostromo/ui-tw/themes/nostromo.css'
 import '../styles/globals.css'
 import '../styles/themes.css'
 
-// Override MDX components
-const mdxComponents: MDXComponents = {
-  pre: (props: any) => {
-    // Extract code from pre > code structure
-    const codeProps = props.children?.props || {}
-    return (
-      <CodeBlock 
-        className={codeProps.className || ''}
-        data-language={codeProps['data-language']}
-      >
-        {codeProps.children || props.children}
-      </CodeBlock>
-    )
-  },
-}
-
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} components={mdxComponents} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
